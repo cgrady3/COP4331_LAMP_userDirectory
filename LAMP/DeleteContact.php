0afffc53e4632ad7b1835261b62570e5fa5dc171
Nathan Guile <37986009+Gurder29@users.noreply.github.com>
2021-05-28 23:33:48 -0400

    Add files via upload
    A rudimentary delete API, or at least how I think one is done.

---
+++ LAMP/DeleteContact.php
@@ -0,0 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("DELETE from Contacts where UserID =?");
+		$stmt->bind_param("s", $inData["UserID"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
