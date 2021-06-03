6ebe1108936f1a2e12a44022d75acbacab98b4b6
Tyler Pearson <pearsonty9@gmail.com>
2021-06-03 11:31:27 -0400

    updated modals and added create, delete and edit functions

--- js/contacts.js
+++ js/contacts.js
@@ -1,30 +1,69 @@
 const input = document.getElementById("searchBox");
-const createContactBtn = document.getElementById("create-contact-btn");
+const addContactBtn = document.getElementById("add-contact-btn");
+const editContactBtn = document.getElementById("edit-contact-btn");
+const deleteContactBtn = document.getElementById("delete-contact-btn");
+const signOutBtn = document.getElementById("signOut-Btn")
 const row = document.getElementById("row-1");
 
 var contactCards = [];
 
 input.addEventListener("input", updateSearch);
-createContactBtn.addEventListener("click", createContact);
+addContactBtn.addEventListener("click", addContact);
+editContactBtn.addEventListener("click", editContact);
+deleteContactBtn.addEventListener("click", deleteContact);
+signOutBtn.addEventListener("click", doLogout);
 
-var contact = {firstName:"John", lastName:"Doe", email:"asdasdasd@asdasdasdasd.com", phone:"12323534234", dateCreated:"10-2-2021"};
+var contact = {firstName:"John", lastName:"Doe", email:"frontendUI@project.com", phone:"812-123-1231", dateCreated:"10-2-2021"};
 
 function updateSearch(e) {
   addCard(contact);
 }
 
-function createContact(){
-  // Get information from modal
-    // UserID
-    // First name
-    // Last name
-    // Email
-    // Phone number
-    // Date created
+function addContact(){
+  const firstName = document.getElementById("add-contact-firstName").value;
+  const lastName = document.getElementById("add-contact-lastName").value;
+  const email = document.getElementById("add-contact-email").value;
+  const number = document.getElementById("add-contact-number").value;
+
+  const contact = {
+    UserId: 0,
+    firstName: firstName,
+    lastName : lastName,
+    email: email,
+    phone : number,
+  }
+
+  console.log(contact);
+  addCard(contact);
+  $('#addModal').modal('hide');
 
   // Send information to api
   
   // Close modal
+}
+
+function editContact(){
+  const firstName = document.getElementById("edit-contact-firstName").value;
+  const lastName = document.getElementById("edit-contact-lastName").value;
+  const email = document.getElementById("edit-contact-email").value;
+  const number = document.getElementById("edit-contact-number").value;
+
+  const contact = {
+    UserId: 0,
+    firstName: firstName,
+    lastName : lastName,
+    email: email,
+    phone : number,
+  }
+
+  console.log(contact);
+  $('#editModal').modal('hide');
+
+  // Turn contact object into JSON and send to api
+}
+
+function deleteContact(){
+
 }
 
 function addCard(contact){
@@ -36,16 +75,16 @@ function addCard(contact){
   header[0].innerText = contact.firstName + " " + contact.lastName;
 
   var body = clone.querySelectorAll("li");
-  body[0].textContent += contact.id;
   body[1].textContent += contact.email;
   body[2].textContent += contact.phone;
 
   var footer = clone.getElementsByClassName("card-footer");
   footer[0].innerText = "Date Created: " + contact.dateCreated;
 
-  clone.addEventListener("click", editCard);
+  clone.addEventListener("click", updateEditModal);
 
   contactCards.push(clone);
+  contacts.push
   row.appendChild(clone);
 }
 
@@ -53,9 +92,19 @@ function deleteCards(){
   var length = contactCards.length;
   for (var i = 0; i < length; i++){
     row.removeChild(contactCards.shift());
+    numContacts--;
   }
 }
 
-function editCard(){
-  
+function updateEditModal(){
+  var header = this.getElementsByClassName("card-header");
+  var body = this.querySelectorAll("li");
+
+  var name = header[0].innerText.split(" ");
+
+  contactId = this.getElementById("contactId").innerText;
+  document.getElementById("edit-contact-firstName").value = name[0];
+  document.getElementById("edit-contact-lastName").value = name[1];
+  document.getElementById("edit-contact-email").value = body[1].innerText.split(" ")[1];
+  document.getElementById("edit-contact-number").value = body[2].innerText.split(" ")[2];
 }
