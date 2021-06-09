0ff4868d313035dbe497baef01b0e203cd23261b
kingcolston <44851415+kingcolston@users.noreply.github.com>
2021-06-08 23:23:49 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,15 +11,16 @@ function loginUser() {
   FirstName = "";
   LastName = "";
 
-  var login = $("#user-Email").val().trim().toLowerCase();
-  var Password = $("#user-Password").val().trim();
+  var form = document.getElementById("login-form");
+  function handleForm(event) { event.preventDefault(); } 
+  form.addEventListener('submit', handleForm);
+  var login = $("#user-email").val().trim().toLowerCase();
+  var Password = $("#user-password").val().trim();
   // hashing password
   Password = md5(Password);
 
-  console.log("Login: " + login + "    Password: " + Password);
-
   var jsonPayload =
-    '{"login" : "' + login + '", "Password" : "' + Password + '"}';
+    '{"Email" : "' + login + '", "Password" : "' + Password + '"}';
   var url = urlBase + "/Login" + extension;
 
   var xhr = new XMLHttpRequest();
@@ -28,21 +29,15 @@ function loginUser() {
   try {
     xhr.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
-        console.log("Login info valid");
-        console.log("Login:" + login + "    Password: " + Password);
-        var jsonObject = JSON.stringify(xhr.responseText);
+        var jsonObject = JSON.parse(xhr.responseText);
         UserID = jsonObject.UserID;
 
-        if (UserID < 1) {
-          $("loginResult").innerHTML = "User/Password combination incorrect";
+        if (jsonObject.error === "User Name / Password do not match OR user does not exist") {
+          alert("Invalid Email/Password");
+          location.reload();
           return;
         }
-
-        FirstName = jsonObject.FirstName;
-        LastName = jsonObject.LastName;
-
         saveCookie();
-
         window.location.href = "/pages/contact.html";
       }
     };
@@ -60,11 +55,13 @@ function signUp() {
   Email = "";
   var error = false;
 
-  var Email = $("#user-Email").val().trim().toLowerCase();
-  var Password = $("#user-Password").val().trim();
+  var Email = $("#user-email").val().trim().toLowerCase();
+  var Password = $("#user-password").val().trim();
   FirstName = $("#first-name").val().trim().toLowerCase();
   LastName = $("#last-name").val().trim().toLowerCase();
-
+  var form = document.getElementById("login-form");
+  function handleForm(event) { event.preventDefault(); } 
+  form.addEventListener('submit', handleForm);
   // validating password length
   if (Password.length < 8 || Password.length > 15) {
     alert("Password must be 8-15 characters long");
@@ -108,13 +105,14 @@ function signUp() {
   try {
     xhr.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
-        console.log("SignUp Info Valid");
-        console.log("Login:" + Email + "    Password: " + Password);
-        var jsonObject = JSON.stringify(xhr.responseText);
-
+        var jsonObject = JSON.parse(xhr.responseText);
+        if (jsonObject.error === "User Already Exists")
+        {
+          alert(jsonObject.error);
+          location.reload();
+          return;
+        }
         UserID = jsonObject.UserID;
-        FirstName = jsonObject.FirstName;
-        LastName = jsonObject.LastName;
 
         saveCookie();
 
@@ -122,7 +120,6 @@ function signUp() {
       }
     };
     xhr.send(jsonPayload);
-    console.log("Login info sent");
   } catch (err) {
     // what is this displaying???
     alert(err);
