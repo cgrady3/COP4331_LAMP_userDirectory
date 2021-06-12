5e885b99322ee6949721ec9824a427b6935ea3f8
Carolyn Grady <owenv8@gmail.com>
2021-06-12 17:07:40 -0400

    update user php string input number fixed

--- API/UpdateUser.php
+++ API/UpdateUser.php
@@ -6,7 +6,7 @@
 	$inData = getRequestInfo();
 		
 	$stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Email=?, Password=?, WHERE UserID=?");
-	$stmt->bind_param("sssssss", $inData["FirstName"], $inData["LastName"], $inData["Email"], $inData["Password"], $inData["UserID"]);
+	$stmt->bind_param("sssss", $inData["FirstName"], $inData["LastName"], $inData["Email"], $inData["Password"], $inData["UserID"]);
 	$stmt->execute();
 
 	returnWithInfo($stmt->affected_rows);
