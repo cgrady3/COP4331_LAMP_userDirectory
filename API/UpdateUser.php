97ba19a36fe1f41026250200df7fd648e6f4d376
Carolyn Grady <owenv8@gmail.com>
2021-06-13 18:01:35 -0400

    strcmp for if email updates, all situations of email or pass update in php

--- API/UpdateUser.php
+++ API/UpdateUser.php
@@ -10,16 +10,25 @@
 	$stmt->execute();
 
 	$result = $stmt->get_result();
-
+	// check if email was updated but already exists in database
 	if (!strcmp($inData["Email"], "false") && $result->num_rows > 0){
 		returnWithError("User Email Already Registered");
 	}
-	else if (!strcmp($inData["Password"], "false")){
+	//check if password was updated by email wasnt
+	else if (strcmp($inData["Password"], "false") && !strcmp($inData["Email"], "false")){
+		$stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Password=? WHERE UserID=?");
+		$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Password"], $inData["UserID"]);
+		$stmt->execute();
+		returnWithInfo($stmt->affected_rows);
+	}
+	// check if email was updated but password wasnt
+	else if (!strcmp($inData["Password"], "false") && strcmp($inData["Email"], "false")){
 		$stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Email=? WHERE UserID=?");
 		$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Email"], $inData["UserID"]);
 		$stmt->execute();
 		returnWithInfo($stmt->affected_rows);
 	}
+	// if both were updated
 	else{
 		$stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Email=?, Password=? WHERE UserID=?");
 		$stmt->bind_param("sssss", $inData["FirstName"], $inData["LastName"], $inData["Email"], $inData["Password"], $inData["UserID"]);
@@ -27,8 +36,6 @@
 		returnWithInfo($stmt->affected_rows);
 	}
 	
-	
-
 	$stmt->close();
 	$conn->close();
 ?>
